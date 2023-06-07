b1266f22f7d0edb28327598a3da96758aebac3ee
enomoto <uraraenomoto@gmail.com>
2023-06-07 16:58:11 +0900

    gallery

---
+++ mafumi-website/components/Gallery.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import styles from '../styles/Gallery.module.scss'
+
+export default function Gallery() {
+  return (
+    <section className='sectionArea'>
+      <div className={styles.galleryWrap}>
+        <figure><Image src="/gallery/photo_img_01.jpg" alt="" fill cover /></figure>
+        <figure><Image src="/gallery/photo_img_02.jpg" alt="" fill cover /></figure>
+        <figure><Image src="/gallery/photo_img_03.jpg" alt="" fill cover /></figure>
+        <figure><Image src="/gallery/photo_img_04.jpg" alt="" fill cover /></figure>
+        <figure><Image src="/gallery/photo_img_05.jpg" alt="" fill cover /></figure>
+        <figure><Image src="/gallery/photo_img_06.jpg" alt="" fill cover /></figure>
+      </div>
+    </section>
+  )
+}
